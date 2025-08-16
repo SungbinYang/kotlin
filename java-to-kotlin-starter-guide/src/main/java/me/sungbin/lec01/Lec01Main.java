1db2232eaa51f9a2125f320a2ca723aeaa91e8d2
SungbinYang <ysb05222@naver.com>
2025-08-16 14:12:20 +0900

    코틀린에서 변수를 다루는 방법

---
+++ java-to-kotlin-starter-guide/src/main/java/me/sungbin/lec01/Lec01Main.java
@@ -0,0 +1,17 @@
+package me.sungbin.lec01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lec01Main {
+    public static void main(String[] args) {
+        long number1 = 10L;
+        final long number2 = 10L;
+
+        Long number3 = 1_000L;
+        Person person = new Person("양성빈");
+
+        List<Integer> list = Arrays.asList(1, 2);
+        list.add(3);
+    }
+}
---
+++ java-to-kotlin-starter-guide/src/main/kotlin/me/sungbin/lec01/Lec01Main.kt
@@ -0,0 +1,24 @@
+package me.sungbin.lec01
+
+fun main() {
+    var number1: Long = 10L
+    number1 = 5L
+
+    val number2 = 10L
+//    number2 = 5L
+
+    var number3: Long
+    number3 = 5L
+    println(number3)
+
+    val number4: Long
+    number4 = 5L
+    println(number4)
+
+    var number5 = 10L
+
+    var number6: Long? = 1_000L
+    number6 = null
+
+    val person = Person("양성빈")
+}
